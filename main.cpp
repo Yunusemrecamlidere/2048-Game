5ab9e12c781ba461fb40007239111e58f2cc10c3
Yunus Emre Camlidere <yunusemrecamlidere08@gmail.com>
2026-05-10 14:58:38 +0300

    4x4 tahta yapisi olusturuldu ve ekrana yazdirildi

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 using namespace std;
+int board[4][4];
+
+void printBoard() {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << board[i][j] << "\t";
+        }
+        cout << endl;
+    }
+}
 
 int main() {
     cout << "2048 Oyunu - Baslangic" << endl;
-    return 0;
+
+for (int i = 0; i < 4; i++)
+        for (int j = 0; j < 4; j++)
+            board[i][j] = 0;
+
+    printBoard();
+
+
+   return 0;
 }
