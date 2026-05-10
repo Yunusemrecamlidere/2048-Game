995d8f8132c06dd1ab5134c68dd8e035aa852e63
Yunus Emre Camlidere <yunusemrecamlidere08@gmail.com>
2026-05-10 15:18:52 +0300

    rastgele 2 ve 4 ekleme fonksiyonu tamamlandi

--- main.cpp
+++ main.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
 using namespace std;
 int board[4][4];
 
@@ -11,6 +14,20 @@ void printBoard() {
     }
 }
 
+void addRandom() {
+    int r, c;
+    do {
+        r = rand() % 4;
+        c = rand() % 4;
+    } while (board[r][c] != 0);
+    
+    int sayi = rand() % 10 + 1;
+    if (sayi == 10)
+        board[r][c] = 4;
+    else
+        board[r][c] = 2;
+}
+
 int main() {
     cout << "2048 Oyunu - Baslangic" << endl;
 
@@ -18,8 +35,11 @@ for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             board[i][j] = 0;
 
+srand(time(0));
+addRandom();
+addRandom();
+
     printBoard();
 
-
-   return 0;
+  return 0;
 }
