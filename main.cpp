dafc580b3b3b5fdfb4c923f7fd7819d5e0d8634f
Yunus Emre Camlidere <yunusemrecamlidere08@gmail.com>
2026-05-11 15:55:14 +0300

    sola kaydirma mantigi eklendi

--- main.cpp
+++ main.cpp
@@ -4,6 +4,51 @@
 
 using namespace std;
 int board[4][4];
+int yedeksa[4];
+
+void solakaydir(){
+
+for (int i=0;i<4;i++){
+   for (int j = 0; j < 4; j++) {
+      yedeksa[j]=board[i][j];
+       }
+       for (int t=0;t<3;t++){// bu döngü sola kaydırıyor.
+         for (int k=0;k<3;k++){
+         if (yedeksa[k]==0){
+           yedeksa[k]=yedeksa[k+1];
+           yedeksa[k+1]=0;}
+         }
+        }
+            for (int k=0;k<3;k++){// bu döngü sola kaymış olanı topluyor. 
+           if(yedeksa[k]==yedeksa[k+1]){
+            yedeksa[k]*=2;
+            yedeksa[k+1]=0;
+           }
+        }
+     for (int t=0;t<3;t++){// bu döngüde toplanmış olanları tekrar sola kaydırıyor.
+   for (int k=0;k<3;k++){
+      if (yedeksa[k]==0){
+         yedeksa[k]=yedeksa[k+1];
+         yedeksa[k+1]=0;
+      }
+   }
+}
+       
+          for (int j=0; j<4; j++){// bu da alt satıra geçmeden doğru satırı boarda ekliyor.
+    board[i][j]=yedeksa[j];
+}
+         }
+    }
+  
+     
+   
+       
+
+
+ 
+
+
+
 
 void printBoard() {
     for (int i = 0; i < 4; i++) {
